83d53d0b81eaf3d403a36207722d25f9e00baac0
matheusrodrigues1 <matech00@outlook.com>
2024-01-30 17:29:13 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <div className="flex font-extrabold pl-7 items-stretch text-5xl flex-col justify-center h-screen w-screen">
+      <div>
+        <h1>SOFTWARE</h1>
+      </div>
+      <div>
+        <h1>DEVELOPER</h1>
+      </div>
+      <div className="w-80">
+        <p className="font-normal text-[0.65rem]  text-left leading-3">TENHO 23 ANOS SOU DESENVOLVEDOR FULLSTACK COM 2 ANO DE EXPERIÊNCIA TRABALHANDO COMO FREELANCER.</p>
+      </div>
+    </div>
+    </>
   );
 }
