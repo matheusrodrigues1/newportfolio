9e05a81659f76d01264a8dd6e3bbf968fcf29554
matheusrodrigues1 <matech00@outlook.com>
2024-01-31 18:51:46 -0300

    update

---
+++ app/projects/page.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import banner from '@/public/Banner Fones.png';
+
+const Projects = () => {
+  return (
+    <>
+        <div className="flex items-center justify-center min-h-screen container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+            <div className="card">
+              <div className="p-5 flex flex-col">
+                <div className="rounded-xl overflow-hidden">
+                  <Image src={banner} alt="banner"/>
+                </div>
+                <h5 className="text-2xl md:text-3xl font-medium mt-3">Project 1</h5>
+                <p className="text-slate-500 text-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum ut dolorum veritatis labore doloribus sapiente, ratione facere expedita fugit a maiores in repudiandae amet, minus sit minima! Accusamus, impedit fugiat?</p>
+                <a href="#" className="text-center bg-blue-400 text-blue-700 py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+              </div>
+            </div>
+            <div className="card">
+              <div className="p-5 flex flex-col">
+                <div className="rounded-xl overflow-hidden">
+                  <Image src={banner} alt="banner"/>
+                </div>
+                <h5 className="text-2xl md:text-3xl font-medium mt-3">Project 1</h5>
+                <p className="text-slate-500 text-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum ut dolorum veritatis labore doloribus sapiente, ratione facere expedita fugit a maiores in repudiandae amet, minus sit minima! Accusamus, impedit fugiat?</p>
+                <a href="#" className="text-center bg-blue-400 text-blue-700 py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+              </div>
+            </div>
+            <div className="card">
+              <div className="p-5 flex flex-col">
+                <div className="rounded-xl overflow-hidden">
+                  <Image src={banner} alt="banner"/>
+                </div>
+                <h5 className="text-2xl md:text-3xl font-medium mt-3">Project 1</h5>
+                <p className="text-slate-500 text-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum ut dolorum veritatis labore doloribus sapiente, ratione facere expedita fugit a maiores in repudiandae amet, minus sit minima! Accusamus, impedit fugiat?</p>
+                <a href="#" className="text-center bg-blue-400 text-blue-700 py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+              </div>
+            </div>
+            <div className="card">
+              <div className="p-5 flex flex-col">
+                <div className="rounded-xl overflow-hidden">
+                  <Image src={banner} alt="banner"/>
+                </div>
+                <h5 className="text-2xl md:text-3xl font-medium mt-3">Project 1</h5>
+                <p className="text-slate-500 text-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum ut dolorum veritatis labore doloribus sapiente, ratione facere expedita fugit a maiores in repudiandae amet, minus sit minima! Accusamus, impedit fugiat?</p>
+                <a href="#" className="text-center bg-blue-400 text-blue-700 py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Explore</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+}
+ 
+export default Projects;
