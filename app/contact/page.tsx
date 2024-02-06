12a82a32d4e6e5636c030a4b74793ff11e828c06
matheusrodrigues1 <matech00@outlook.com>
2024-02-06 19:57:04 -0300

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -17,15 +17,15 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex pt-5 md:pt-3 border-t-2 w-full h-64 flex-col items-center">
+    <div className="flex pt-5 md:pt-3 lg:pt-10 border-t-2 w-full h-64 lg:h-80 flex-col items-center">
       <Image src={Newsletter} alt='newsletter' />
-      <h1 className="font-bold mb-3 md:mb-5 md:text-xl">Me mande um email</h1>
-      <div className={`flex bg-gray-200 p-4 md:w-[32rem] justify-center rounded md:rounded-b-none font-medium ${copiado ? 'bg-teal-300' : ''}`}>
+      <h1 className="font-bold mb-3 md:mb-5 lg:mb-8 md:text-xl lg:text-2xl">Me mande um email</h1>
+      <div className={`flex bg-gray-200 p-4 md:w-[32rem] lg:w-[40rem] justify-center rounded md:rounded-b-none font-medium ${copiado ? 'bg-teal-300' : ''}`}>
         <h4>{email}</h4>
       </div>
       <button
         onClick={copiarEmail}
-        className="mt-0 px-4 py-2 md:w-[32rem] bg-blue-500 text-white rounded md:rounded-t-none hover:bg-blue-600"
+        className="mt-0 px-4 py-2 md:w-[32rem] lg:w-[40rem] bg-blue-500 text-white rounded md:rounded-t-none hover:bg-blue-600"
       >
         {copiado ? 'EMAIL COPIADO!' : 'COPIAR EMAIL'}
       </button>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -17,30 +17,30 @@ const Home = () => {
   }, []);
   return (
     <>
-    <div className="flex font-extrabold pl-7 pt-40 md:pt-20 text-5xl flex-col justify-start md:items-center h-screen w-screen">
-      <div className="flex flex-row items-center pl-20 md:pl-0 gap-2 md:gap-[1.18rem] mb-3 md:mb-8 text-zinc-700">
+    <div className="flex font-extrabold pl-7 pt-40 md:pt-20 text-5xl flex-col justify-start md:items-center md:justify-center lg:items-center lg:justify-center h-screen w-screen">
+      <div className="flex flex-row items-center pl-20 md:pl-0 gap-2 md:gap-[1.18rem] lg:gap-[0.7rem] mb-3 md:mb-8 text-zinc-700">
           <a href="https://www.linkedin.com/in/matheus-rodrigues-1a1899231/" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon className="w-7 md:w-10 transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faLinkedin} />
+            <FontAwesomeIcon className="w-7 md:w-10 lg:w-16 transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faLinkedin} />
           </a>
           <a href="https://github.com/matheusrodrigues1" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon className="w-7 md:w-10 transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faGithub} />
+            <FontAwesomeIcon className="w-7 md:w-10 lg:w-16 transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faGithub} />
           </a>
           <a href="https://drive.google.com/file/d/1Jfn9RAqFR3YaQbL8j_lJA0z8HHlLI3Xq/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon className="w-[1.4rem] md:w-[1.8rem] transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faFileLines} />
+            <FontAwesomeIcon className="w-[1.4rem] md:w-[1.8rem] lg:w-[2rem] transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faFileLines} />
           </a>
           <a href="https://drive.google.com/file/d/1Jfn9RAqFR3YaQbL8j_lJA0z8HHlLI3Xq/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon className="w-7 md:w-10 transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faInstagram} />
+            <FontAwesomeIcon className="w-7 md:w-10 lg:w-16 transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faInstagram} />
           </a>
       </div>
-      <div className='md:text-[7rem] md:font-extrabold'>
-        <h1 className='mb-[-10px] md:mb-[-23px]'>SOFTWARE</h1>
+      <div className='md:text-[7rem] lg:text-[8.6rem] md:font-extrabold'>
+        <h1 className='mb-[-10px] md:mb-[-23px] lg:mb-[-35px]'>SOFTWARE</h1>
         <h1>DEVELOPER</h1>
       </div>
-      <div className="w-80 md:w-[27rem] md:ml-[17rem] mt-1 md:mt-0">
-        <p className="font-normal md:font-semibold text-[0.65rem] md:text-sm text-left leading-3">TENHO 23 ANOS SOU DESENVOLVEDOR FULLSTACK COM 2 ANOS DE EXPERIÊNCIA TRABALHANDO COMO FREELANCER.</p>
+      <div className="w-80 md:w-[27rem] lg:w-[32rem] md:ml-[17rem] lg:ml-[25rem] mt-1 md:mt-0">
+        <p className="font-normal md:font-semibold text-[0.65rem] md:text-sm lg:text-base text-left leading-3">TENHO 23 ANOS SOU DESENVOLVEDOR FULLSTACK COM 2 ANOS DE EXPERIÊNCIA TRABALHANDO COMO FREELANCER.</p>
       </div>
       <div className="flex items-end md:items-center pt-[8rem] md:pt-[4.5rem] pl-[9rem] md:pl-0">
-          <FontAwesomeIcon className={`text-teal-500 w-9 text-6xl mr-2 cursor-pointer transition-transform transform ${
+          <FontAwesomeIcon className={`text-teal-500 w-9 lg:w-11 text-6xl mr-2 cursor-pointer transition-transform transform ${
           isIconUp ? '-translate-y-1' : 'translate-y-1'
         }`} icon={faCircleDown} />
       </div>
