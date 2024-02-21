a0b2bac736e8adec29a6a2eff7a18765929bbbb1
matheusrodrigues1 <matech00@outlook.com>
2024-02-21 17:17:06 -0300

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,7 +7,6 @@ import banner4 from '@/public/04.png';
 const Projects = () => {
   return (
     <>
-        <h1 className="mt-10">projetos</h1>
         <div className="flex items-center justify-center min-h-screen container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="card">
