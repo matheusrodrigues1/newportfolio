91ba74cb7c6c8daa2e78ee2fa600ef1afd43428d
matheusrodrigues1 <matech00@outlook.com>
2024-02-21 19:36:29 -0300

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '319px',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
