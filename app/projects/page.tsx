64b4aeb67a06543dfb894f55c902219672a0fa84
matheusrodrigues1 <matech00@outlook.com>
2024-02-21 16:33:48 -0300

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
-import banner from '@/public/Banner Fones.png';
+import banner from '@/public/01.png';
+import banner2 from '@/public/02.png';
+import banner3 from '@/public/03.png';
+import banner4 from '@/public/04.png';
 
 const Projects = () => {
   return (
@@ -20,7 +23,7 @@ const Projects = () => {
             <div className="card">
               <div className="p-5 flex flex-col">
                 <div className="rounded-xl overflow-hidden">
-                  <Image src={banner} alt="banner"/>
+                  <Image src={banner2} alt="banner"/>
                 </div>
                 <h5 className="text-2xl md:text-3xl font-medium mt-3">Project 1</h5>
                 <p className="text-slate-500 text-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum ut dolorum veritatis labore doloribus sapiente, ratione facere expedita fugit a maiores in repudiandae amet, minus sit minima! Accusamus, impedit fugiat?</p>
@@ -30,7 +33,7 @@ const Projects = () => {
             <div className="card">
               <div className="p-5 flex flex-col">
                 <div className="rounded-xl overflow-hidden">
-                  <Image src={banner} alt="banner"/>
+                  <Image src={banner3} alt="banner"/>
                 </div>
                 <h5 className="text-2xl md:text-3xl font-medium mt-3">Project 1</h5>
                 <p className="text-slate-500 text-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum ut dolorum veritatis labore doloribus sapiente, ratione facere expedita fugit a maiores in repudiandae amet, minus sit minima! Accusamus, impedit fugiat?</p>
@@ -40,7 +43,7 @@ const Projects = () => {
             <div className="card">
               <div className="p-5 flex flex-col">
                 <div className="rounded-xl overflow-hidden">
-                  <Image src={banner} alt="banner"/>
+                  <Image src={banner4} alt="banner"/>
                 </div>
                 <h5 className="text-2xl md:text-3xl font-medium mt-3">Project 1</h5>
                 <p className="text-slate-500 text-lg mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum ut dolorum veritatis labore doloribus sapiente, ratione facere expedita fugit a maiores in repudiandae amet, minus sit minima! Accusamus, impedit fugiat?</p>
