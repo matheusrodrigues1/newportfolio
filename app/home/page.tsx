17180183de28734adc84d00df6fc808bf61d8180
matheusrodrigues1 <matech00@outlook.com>
2024-02-21 19:46:49 -0300

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -32,12 +32,12 @@ const Home = () => {
             <FontAwesomeIcon className="w-7 md:w-10 lg:w-16 transition-transform transform hover:-translate-y-1 hover:text-teal-500" icon={faInstagram} />
           </a>
       </div>
-      <div className='md:text-[7rem] lg:text-[8.6rem] md:font-extrabold'>
+      <div className='text-black md:text-[7rem] lg:text-[8.6rem] md:font-extrabold'>
         <h1 className='mb-[-10px] md:mb-[-23px] lg:mb-[-35px]'>SOFTWARE</h1>
         <h1>DEVELOPER</h1>
       </div>
       <div className="w-80 md:w-[27rem] lg:w-[32rem] md:ml-[17rem] lg:ml-[25rem] mt-1 md:mt-0">
-        <p className="font-normal md:font-semibold text-[0.65rem] md:text-sm lg:text-base text-left leading-3">TENHO 23 ANOS SOU DESENVOLVEDOR FULLSTACK COM 2 ANOS DE EXPERIÊNCIA TRABALHANDO COMO FREELANCER.</p>
+        <p className="font-normal text-black md:font-semibold text-[0.65rem] md:text-sm lg:text-base text-left leading-3">TENHO 23 ANOS SOU DESENVOLVEDOR FULLSTACK COM 2 ANOS DE EXPERIÊNCIA TRABALHANDO COMO FREELANCER.</p>
       </div>
       <div className="flex items-end md:items-center pt-[8rem] md:pt-[4.5rem] pl-[9rem] md:pl-0">
           <FontAwesomeIcon className={`text-teal-500 w-9 lg:w-11 text-6xl mr-2 cursor-pointer transition-transform transform ${
