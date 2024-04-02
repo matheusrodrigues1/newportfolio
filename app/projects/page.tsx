5889c2fe5d111d017149536e7d0006c645ac79b6
matheusrodrigues1 <matech00@outlook.com>
2024-04-02 19:38:19 -0300

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,8 +18,8 @@ const Projects = () => {
                 <h5 translate="no" className="text-2xl text-white md:text-3xl font-medium mt-3">Imaginify</h5>
                 <p className="text-[#DCDCDC] text-sm mt-3">Uma plataforma de imagens de IA destacada que se destaca em recursos de processamento de imagens, integra uma infraestrutura de pagamento segura, oferece funcionalidades avançadas de pesquisa de imagens e suporta vários recursos de IA, incluindo restauração de imagens, recoloração, remoção de objetos, pintura generativa e remoção de fundo.</p>
                 <p className="text-white text-md mt-2 he">Tecnologias: <span className="text-[#FF4500]">NextJS</span>, <span  className="text-[#1E90FF]">Typescript</span>, <span className="text-[#4682B4]">Tailwindcss</span>, <span className="text-[#00FF00]">MongoDB</span> e <span className="text-[#1E90FF]">Stripe</span>.</p>
-                <a href="https://github.com/matheusrodrigues1/Imaginefy" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-9 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Github Repository</a>
-                <a href="https://imaginefy.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Project Link</a>
+                <a href="https://github.com/matheusrodrigues1/Imaginefy" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-9 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Repositório</a>
+                <a href="https://imaginefy.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Link</a>
               </div>
             </div>
             <div className="card">
@@ -30,8 +30,8 @@ const Projects = () => {
                 <h5 translate="no" className="text-2xl text-white md:text-3xl font-medium mt-3">SnapchatApp</h5>
                 <p className="text-[#DCDCDC] text-sm mt-3">O projeto snapchatapp é um aplicativo que visa replicar as principais funcionalidades do app Snapchat, disponibilizando uma plataforma para troca de mensagens instantâneas e multimídia entre os usuários.</p>
                 <p className="text-white text-md mt-2 he">Tecnologias: <span className="text-[#FF4500]">NextJS</span>, <span  className="text-[#1E90FF]">Typescript</span>, <span className="text-[#4682B4]">Tailwindcss</span> e <span className="text-[#00FF00]">MongoDB</span>.</p>
-                <a href="https://github.com/matheusrodrigues1/snapchatapp" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-9 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Github Repository</a>
-                <a href="https://snapchatapp.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Project Link</a>
+                <a href="https://github.com/matheusrodrigues1/snapchatapp" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-9 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Repositório</a>
+                <a href="https://snapchatapp.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Link</a>
               </div>
             </div>
             <div className="card">
@@ -42,8 +42,8 @@ const Projects = () => {
                 <h5 translate="no" className="text-2xl text-white md:text-3xl font-medium mt-3">Evently</h5>
                 <p className="text-[#DCDCDC] text-sm mt-3">Evently é uma plataforma de eventos online que oferece inscrição e participação em diversas atividades. Com integrações de Next.js, Stripe, Clerk e MongoDB, juntamente com o uso da estrutura Tailwind CSS, fornece uma experiência simplificada e segura para organizadores e participantes.</p>
                 <p className="text-white text-md mt-2 he">Tecnologias: <span className="text-[#FF4500]">NextJS</span>, <span className="text-[#1E90FF]">Typescript</span>, <span className="text-[#4682B4]">Tailwindcss</span>, <span className="text-[#00FF00]">MongoDB</span>, <span className="text-[#FF69B4]">Stripe</span> e <span className="text-[#7B68EE]">Clerk</span>.</p>
-                <a href="https://github.com/matheusrodrigues1/evently" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Github Repository</a>
-                <a href="https://evently-chi-two.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Project Link</a>
+                <a href="https://github.com/matheusrodrigues1/evently" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Repositório</a>
+                <a href="https://evently-chi-two.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Link</a>
               </div>
             </div>
             <div className="card">
@@ -54,8 +54,8 @@ const Projects = () => {
                 <h5 translate="no" className="text-2xl text-white md:text-3xl font-medium mt-3">Study.Async</h5>
                 <p className="text-[#DCDCDC] text-sm mt-3">Plataforma de estudo interativa para diversas áreas do conhecimento.</p>
                 <p className="text-md mt-2 he text-white">Tecnologias: <span className="text-[#00FF00]">Python</span>, <span className="text-[#228B22]">Django</span> e <span className="text-[#1E90FF]">SQLite</span>.</p>
-                <a href="https://github.com/matheusrodrigues1/study.Async" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-[7.5rem] hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Github Repository</a>
-                <a href="https://github.com/matheusrodrigues1/study.Async" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Project Link</a>
+                <a href="https://github.com/matheusrodrigues1/study.Async" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-[7.5rem] hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Repositório</a>
+                <a href="https://github.com/matheusrodrigues1/study.Async" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Link</a>
               </div>
             </div>
             <div className="card">
@@ -66,8 +66,8 @@ const Projects = () => {
                 <h5 translate="no" className="text-2xl text-white md:text-3xl font-medium mt-3">Mecajato</h5>
                 <p className="text-[#DCDCDC] text-sm mt-3">Um sistema de ordens de serviço desenvolvido em Python e Django, utilizando SQLite como banco de dados. Os clientes podem selecionar serviços, como troca de óleo, definir a data de retirada e visualizar o custo. Os funcionários podem gerar PDFs com detalhes do pedido, como placa do veículo, nome do cliente e custo do serviço, usando a biblioteca fpdf.</p>
                 <p className="text-md mt-2 he text-white">Tecnologias: <span className="text-[#00FF00]">Python</span>, <span className="text-[#228B22]">Django</span> e <span className="text-[#1E90FF]">SQLite</span>.</p>
-                <a href="https://github.com/matheusrodrigues1/mecajato" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Github Repository</a>
-                <a href="https://github.com/matheusrodrigues1/mecajato" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Project Link</a>
+                <a href="https://github.com/matheusrodrigues1/mecajato" target="_blank" rel="noopener noreferrer" className="text-center bg-white text-black py-2  rounded-lg font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Repositório</a>
+                <a href="https://github.com/matheusrodrigues1/mecajato" target="_blank" rel="noopener noreferrer" className="text-center bg-blue-400 text-black py-2  rounded-lg font-semibold mt-1 hover:bg-blue-300 focus:scale-95 transition-all duration-200 ease-out">Link</a>
               </div>
             </div>
           </div>
