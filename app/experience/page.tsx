65cc05fb4b691ac61f1caf88b06f3dc248d9707c
matheusrodrigues1 <matech00@outlook.com>
2024-11-05 09:35:57 -0300

    update

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -30,9 +30,9 @@ const Experiencia: NextPage = () => {
           </h2>
           <AccordionPanel pb={4} className="pl-3 mt-3">
             <span className="font-bold">
-              Suporte Técnico em TI - Toledo Distribuidor:
+              Suporte Técnico em TI - Toledo Distribuidor
             </span>{" "}
-            Atuação em diversas áreas de suporte técnico.
+            (10/2024 até o momento)
             <div className="ml-5 mt-[-1rem] lg:pr-10">
               <br /> • Suporte técnico em infraestrutura de redes, solucionando
               problemas de conectividade e desempenho.
@@ -51,7 +51,8 @@ const Experiencia: NextPage = () => {
           <AccordionPanel pb={4} className="pl-3 mt-3">
             <span className="font-bold">Starti</span> - 1ª empresa Júnior de
             Tecnologia em Alagoas -{" "}
-            <span className="font-bold">IFAL campus Arapiraca</span>
+            <span className="font-bold">IFAL campus Arapiraca</span> (12/2023
+            até o momento)
             <div className="ml-5 mt-[-1rem] lg:pr-10">
               <br /> • Desenvolvedor de software atuando tanto no frontend
               quanto no backend, atualmente trabalhando com backend utilizando
@@ -59,7 +60,8 @@ const Experiencia: NextPage = () => {
             </div>
           </AccordionPanel>
           <AccordionPanel pb={4} className="pl-3 mt-3">
-            <span className="font-bold">Freelancer</span>
+            <span className="font-bold">Freelancer</span> (08/2022 até o
+            momento)
             <div className="ml-5 mt-[-1rem] lg:pr-28">
               <br /> • Com dois anos de experiência como freelancer na área de
               desenvolvimento full stack, especializei-me em tecnologias como
@@ -74,8 +76,8 @@ const Experiencia: NextPage = () => {
             </div>
           </AccordionPanel>
           <AccordionPanel pb={4} className="pl-3 mt-3">
-            <span className="font-bold">Telemarketing - AeC:</span> Experiência
-            em atendimento ao cliente e vendas.
+            <span className="font-bold">Telemarketing - AeC</span> (05/2020 até
+            05/2024)
             <div className="ml-5 mt-[-1rem] lg:pr-10">
               <br /> • Realização de chamadas para prospecção e atendimento a
               clientes, oferecendo informações sobre produtos e serviços.
